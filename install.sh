f95171d59d3e57ebdb0a0e496c59f5217aad02b4
PrinceOfRavens <jcmg3c@gmail.com>
2022-05-16 00:54:16 -0500

    autorip service upgrade

--- install.sh
+++ install.sh
@@ -40,59 +40,60 @@ mount_share() {
 
 
 install_ffmpeg_makemkv() {
-	echo
-	echo '############################################'
-	echo '##  Installing Libraries and Build Tools  ##'
-	echo '############################################'
-	echo
-	echo "$secretPass" | sudo -S apt install -y build-essential pkg-config libc6-dev libssl-dev libexpat1-dev libavcodec-dev libgl1-mesa-dev qtbase5-dev zlib1g-dev nasm libfdk-aac-dev sed wget curl tar setcd libxml2-utils
-	
-	
-	echo
-	echo '#########################'
-	echo '##  Installing FFMPEG  ##'
-	echo '#########################'
-	echo
-
-	rm -f $userhome/ffmpeg
-	mkdir $userhome/ffmpeg
-	cd $userhome/ffmpeg
-	wget https://ffmpeg.org/releases/ffmpeg-snapshot.tar.bz2
-	tar -xvjf ffmpeg-snapshot.tar.bz2
-	cd ffmpeg
-	./configure --prefix=/tmp/ffmpeg --enable-static --disable-shared --enable-pic --enable-libfdk-aac
-	make install
-
-	latestVersion=$(curl "https://forum.makemkv.com/forum/viewtopic.php?f=3&t=224" -s | xmllint --html --nowarning --xpath "//title/text()" - 2>/dev/null | awk '{print $2}')
-	
-	echo
-	echo '################################'
-	echo "##  Installing MakeMKV ${latestVersion}  ##"
-	echo '################################'
-	echo
-
-	latestVersion=$(curl "https://forum.makemkv.com/forum/viewtopic.php?f=3&t=224" -s | xmllint --html --nowarning --xpath "//title/text()" - 2>/dev/null | awk '{print $2}')
-
-	rm -f $userhome/makemkv
-	mkdir $userhome/makemkv
-	cd $userhome/makemkv
-	wget https://www.makemkv.com/download/makemkv-oss-1.16.5.tar.gz
-	tar -xvzf makemkv-oss-1.16.5.tar.gz
-	cd makemkv-oss-1.16.5
-	PKG_CONFIG_PATH=/tmp/ffmpeg/lib/pkgconfig ./configure
-	make
-	echo "$secretPass" | sudo -S make install
-	
-	cd $userhome/makemkv
-	wget https://www.makemkv.com/download/makemkv-bin-1.16.5.tar.gz
-	tar -xvzf makemkv-bin-1.16.5.tar.gz
-	cd makemkv-bin-1.16.5
-	make
-	echo "$secretPass" | sudo -S make install
-	
-	rm -rf /tmp/ffmpeg
-	
-	echo "$secretPass" | sudo -S apt install default-jre
+	echo "$secretPass" | sudo -S install-makemkv.sh
+#	echo
+#	echo '############################################'
+#	echo '##  Installing Libraries and Build Tools  ##'
+#	echo '############################################'
+#	echo
+#	echo "$secretPass" | sudo -S apt install -y build-essential pkg-config libc6-dev libssl-dev libexpat1-dev libavcodec-dev libgl1-mesa-dev qtbase5-dev zlib1g-dev nasm libfdk-aac-dev sed wget curl tar setcd libxml2-utils
+#	
+#	
+#	echo
+#	echo '#########################'
+#	echo '##  Installing FFMPEG  ##'
+#	echo '#########################'
+#	echo
+#
+#	rm -f $userhome/ffmpeg
+#	mkdir $userhome/ffmpeg
+#	cd $userhome/ffmpeg
+#	wget https://ffmpeg.org/releases/ffmpeg-snapshot.tar.bz2
+#	tar -xvjf ffmpeg-snapshot.tar.bz2
+#	cd ffmpeg
+#	./configure --prefix=/tmp/ffmpeg --enable-static --disable-shared --enable-pic --enable-libfdk-aac
+#	make install
+#
+#	latestVersion=$(curl "https://forum.makemkv.com/forum/viewtopic.php?f=3&t=224" -s | xmllint --html --nowarning --xpath "//title/text()" - 2>/dev/null | awk '{print $2}')
+#	
+#	echo
+#	echo '################################'
+#	echo "##  Installing MakeMKV ${latestVersion}  ##"
+#	echo '################################'
+#	echo
+#
+#	latestVersion=$(curl "https://forum.makemkv.com/forum/viewtopic.php?f=3&t=224" -s | xmllint --html --nowarning --xpath "//title/text()" - 2>/dev/null | awk '{print $2}')
+#
+#	rm -f $userhome/makemkv
+#	mkdir $userhome/makemkv
+#	cd $userhome/makemkv
+#	wget https://www.makemkv.com/download/makemkv-oss-1.16.5.tar.gz
+#	tar -xvzf makemkv-oss-1.16.5.tar.gz
+#	cd makemkv-oss-1.16.5
+#	PKG_CONFIG_PATH=/tmp/ffmpeg/lib/pkgconfig ./configure
+#	make
+#	echo "$secretPass" | sudo -S make install
+#	
+#	cd $userhome/makemkv
+#	wget https://www.makemkv.com/download/makemkv-bin-1.16.5.tar.gz
+#	tar -xvzf makemkv-bin-1.16.5.tar.gz
+#	cd makemkv-bin-1.16.5
+#	make
+#	echo "$secretPass" | sudo -S make install
+#	
+#	rm -rf /tmp/ffmpeg
+#	
+#	echo "$secretPass" | sudo -S apt install default-jre
 }
 
 
@@ -106,21 +107,23 @@ autorip_setup() {
 	echo
 	read -p 'MakeMKV Key: ' licenseKey
 	echo
-
-	#licenseHolder="000000000000"
-	#sed -i "s/$licenseHolder/$licenseKey/" "$scriptroot/settings.cfg"
 	
-	/usr/bin/makemkvcon reg $licenseKey
+	#echo "$secretPass" | sudo -S /usr/bin/makemkvcon reg $licenseKey
 
 	presetDir="~/Videos"
 	sed -i "s/$presetDir/$mountTarget/" "$scriptroot/settings.cfg"
+	sed -i "s/000000000000/$licenseKey/" "$scriptroot/settings.cfg"
 
-	#echo 'apt_Key = "Holder"' | sudo tee -a $userhome/.MakeMKV/update.conf
-
-	chmod +x $scriptroot/wrapper.sh
+	echo "$secretPass" | sudo -S rm -f /usr/share/autorip/*
+	echo "$secretPass" | sudo -S install -d /usr/share/autorip
+	echo "$secretPass" | sudo -S install -m 755 -t /usr/share/autorip src/autorip.sh
+	echo "$secretPass" | sudo -S install -m 755 -t /usr/share/autorip src/install-makemkv.sh
+	echo "$secretPass" | sudo -S install -m 644 -t /usr/share/autorip src/settings.cfg
+	echo "$secretPass" | sudo -S ln -s -f /usr/share/autorip/autorip.sh /usr/bin/autorip
+	echo "$secretPass" | sudo -S ln -s -f /usr/share/autorip/install-makemkv /usr/bin/install-makemkv
 }
 
-
+#!#
 daemon_service() {
 	echo
 	echo '###############################'
@@ -128,15 +131,18 @@ daemon_service() {
 	echo '###############################'
 	echo
 	
-	echo "[Unit]" | sudo tee -a /lib/systemd/system/autorip.service
-	echo "Description=MakeMKV Autorip Script" | sudo tee -a /lib/systemd/system/autorip.service
-	echo "" | sudo tee -a /lib/systemd/system/autorip.service
-	echo "[Service]" | sudo tee -a /lib/systemd/system/autorip.service
-	echo "User=$USER" | sudo tee -a /lib/systemd/system/autorip.service
-	echo "ExecStart=echo \"$secretPass\" | sudo -S $userhome/autorip/wrapper.sh" | sudo tee -a /lib/systemd/system/autorip.service
-	echo "" | sudo tee -a /lib/systemd/system/autorip.service
-	echo "[Install]" | sudo tee -a /lib/systemd/system/autorip.service
-	echo "WantedBy=multi-user.target" | sudo tee -a /lib/systemd/system/autorip.service
+	#echo "[Unit]" | sudo tee -a /lib/systemd/system/autorip.service
+	#echo "Description=MakeMKV Autorip Script" | sudo tee -a /lib/systemd/system/autorip.service
+	#echo "" | sudo tee -a /lib/systemd/system/autorip.service
+	#echo "[Service]" | sudo tee -a /lib/systemd/system/autorip.service
+	#echo "User=$USER" | sudo tee -a /lib/systemd/system/autorip.service
+	#echo "ExecStart=echo \"$secretPass\" | sudo -S $userhome/autorip/wrapper.sh" | sudo tee -a /lib/systemd/system/autorip.service
+	#echo "" | sudo tee -a /lib/systemd/system/autorip.service
+	#echo "[Install]" | sudo tee -a /lib/systemd/system/autorip.service
+	#echo "WantedBy=multi-user.target" | sudo tee -a /lib/systemd/system/autorip.service
+
+	echo "$secretPass" | sudo -S useradd -r -s /bin/false autorip
+	echo "$secretPass" | sudo -S install -m 755 -t /etc/systemd/system src/autorip.service
 
 	echo "$secretPass" | sudo -S systemctl daemon-reload
 	echo "$secretPass" | sudo -S systemctl enable autorip.service
@@ -145,6 +151,6 @@ daemon_service() {
 
 
 mount_share
-install_ffmpeg_makemkv
 autorip_setup
-#daemon_service
+install_ffmpeg_makemkv
+daemon_service
--- src/install-makemkv.sh
+++ src/install-makemkv.sh
@@ -10,12 +10,22 @@ if [ "$USER" != root ] && [ "$SUDO_USER" != root ]; then
 fi
 
 
-if [-d $TMP_PATH]; then
-    rm -rf $TMP_PATH
+if [-d "$TMP_PATH"]; then
+    rm -rf "$TMP_PATH"
 fi
 
-mkdir $TMP_PATH
+mkdir "$TMP_PATH"
 
+
+
+echo
+echo '############################################'
+echo '##  Installing Libraries and Build Tools  ##'
+echo '############################################'
+echo
+apt update
+apt upgrade -y
+apt install -y build-essential pkg-config libc6-dev libssl-dev libexpat1-dev libavcodec-dev libgl1-mesa-dev qtbase5-dev zlib1g-dev nasm libfdk-aac-dev sed wget curl tar setcd
 
 
 
@@ -25,11 +35,11 @@ echo '##  Installing FFMPEG  ##'
 echo '#########################'
 echo
 
-mkdir $TMP_PATH/ffmpeg
-cd $TMP_PATH/ffmpeg
+mkdir "$TMP_PATH/ffmpeg"
+cd "$TMP_PATH/ffmpeg"
 wget https://ffmpeg.org/releases/ffmpeg-snapshot.tar.bz2
 tar -xvjf ffmpeg-snapshot.tar.bz2
-cd ffmpeg
+cd "ffmpeg"
 ./configure --prefix=/tmp/ffmpeg --enable-static --disable-shared --enable-pic --enable-libfdk-aac
 make install
 
@@ -44,21 +54,31 @@ echo "##  Installing MakeMKV ${latestVersion}  ##"
 echo '################################'
 echo
 
-mkdir $TMP_PATH/makemkv
-cd $TMP_PATH/makemkv
+mkdir "$TMP_PATH/makemkv"
+cd "$TMP_PATH/makemkv"
 wget https://www.makemkv.com/download/makemkv-oss-${latestVersion}.tar.gz
 tar -xvzf makemkv-oss-${latestVersion}.tar.gz
-cd makemkv-oss-${latestVersion}
+cd "makemkv-oss-${latestVersion}"
 PKG_CONFIG_PATH=/tmp/ffmpeg/lib/pkgconfig ./configure
 make
 make install
 	
-cd $TMP_PATH/makemkv
+cd "$TMP_PATH/makemkv"
 wget https://www.makemkv.com/download/makemkv-bin-${latestVersion}.tar.gz
 tar -xvzf makemkv-bin-${latestVersion}.tar.gz
-cd makemkv-bin-${latestVersion}
-make
-make install
+cd "makemkv-bin-${latestVersion}"
+#make
+#make install
+rm -f /usr/bin/makemkvcon
+rm -f /usr/bin/mmdtsdec
+rm -f /usr/bin/share/MakeMKV/*
+install -d /usr/share/MakeMKV
+install -d /usr/bin
+install -t /usr/bin bin/amd64/makemkvcon
+install -m 644 -t /usr/share/MakeMKV src/share/appdata.tar
+install -m 644 -t /usr/share/MakeMKV src/share/blues.jar
+install -m 644 -t /usr/share/MakeMKV src/share/blues.policy
+cd /usr/bin && ln -s -f makemkvcon sdftool
 	
-rm -rf /tmp/ffmpeg
-rm -rf $TMP_PATH
+rm -rf "/tmp/ffmpeg"
+rm -rf "$TMP_PATH"
